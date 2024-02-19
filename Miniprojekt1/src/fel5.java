853cadc84952dd61ffefb19bd75918727598d052
Aron <csatabedearonka@gmail.com>
2024-02-19 16:50:11 +0200

    feladat 6 start

--- Miniprojekt1/src/fel5.java
+++ Miniprojekt1/src/fel5.java
@@ -1,17 +1,17 @@
 public class fel5 {
-    public static Boolean csakAlfanumerikus(String string_tesztelni){
+    public static Boolean csakAlfanumerikus(String string_tesztelni) {
         for (int i = 0; i < string_tesztelni.length(); i++) {
             char karakter = string_tesztelni.charAt(i);
-            if(!(karakter>='a' && karakter<='z')&& ! (karakter>='A' && karakter<='Z')){
+            if (!(karakter >= 'a' && karakter <= 'z') && !(karakter >= 'A' && karakter <= 'Z')) {
                 return false;
             }
         }
         return true;
     }
 
-    public static void main(String[] args){
-        String tesztString1= "Helloezegyteszt";
-        String tesztString2= "Hello ez egy teszt 123";
+    public static void main(String[] args) {
+        String tesztString1 = "Helloezegyteszt";
+        String tesztString2 = "Hello ez egy teszt 123";
 
         System.out.println("Test1: " + csakAlfanumerikus(tesztString1));
         System.out.println("Test2: " + csakAlfanumerikus(tesztString2));
