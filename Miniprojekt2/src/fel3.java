5949e59a4b704578063772a8b57867b86192d38a
Aron <csatabedearonka@gmail.com>
2024-02-29 13:10:36 +0200

    Miniprojekt2_fel3

---
+++ Miniprojekt2/src/fel3.java
@@ -0,0 +1,31 @@
+public class fel3 {
+    public static void main(String[] args) {
+        String input = "sssjkllllopww223";
+        String result = removeDuplicates(input);
+        System.out.println("Eredeti: " + input);
+        System.out.println("Eltavolitott duplikaciok: " + result);
+    }
+
+    public static String removeDuplicates(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            boolean alreadyExists = false;
+
+            for (int j = 0; j < sb.length(); j++) {
+                if (sb.charAt(j) == currentChar) {
+                    alreadyExists = true;
+                    break;
+                }
+            }
+
+
+            if (!alreadyExists) {
+                sb.append(currentChar);
+            }
+        }
+
+        return sb.toString();
+    }
+}
