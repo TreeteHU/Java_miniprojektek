acf14507c0c791e650ff2311a900998421b36058
Aron <csatabedearonka@gmail.com>
2024-02-29 13:01:12 +0200

    fel1

--- Miniprojekt2/src/fel1.java
+++ Miniprojekt2/src/fel1.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class fel1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Addj meg egy szot: ");
+        String szo = scanner.nextLine();
+
+        if (szo.length() > 0) {
+            char elsoKarakter = szo.charAt(0);
+            char utolsoKarakter = szo.charAt(szo.length() - 1);
+
+            System.out.println("Az elso karakter: " + elsoKarakter);
+            System.out.println("Az utolso karakter: " + utolsoKarakter);
+        } else {
+            System.out.println("Nem adtal meg egy szot se.");
+        }
+
+        scanner.close();
+    }
 }
