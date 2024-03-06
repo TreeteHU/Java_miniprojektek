434f76c951d4391c36171d9575cf7b3519ede6e2
Aron <csatabedearonka@gmail.com>
2024-03-06 19:09:39 +0200

    fel9

---
+++ Miniprojekt2/src/fel9.java
@@ -0,0 +1,16 @@
+public class fel9 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[10][10];
+
+        for (int i = 0; i < 10; i++) {
+            matrix[i][i] = i + 1;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
